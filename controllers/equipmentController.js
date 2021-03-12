7375782d1036c2849148101fff8ed8d56be2d79b
Rivers Marc <m.t.rivers@hotmail.com>
2021-03-12 07:57:36 -0600

    working on routes

--- controllers/equipmentController.js
+++ controllers/equipmentController.js
@@ -11,6 +11,20 @@ const EquipmentModel = require("../models").Equipment;
     res.json({ allEquipment });
   })
 
+  // Get Single Piece of Equipment
+  router.get("/:id", async (req, res) => {
+    let equip = await EquipmentModel.findByPk(req.params.id);
+    res.json({ equip });
+  });
 
+  // UPDATE Equipment
+router.put("/:id", async (req, res) => {
+  EquipmentModel.update(req.body, {
+    where: { id: req.params.id },
+    returning: true,
+  });
+  console.log("updated")
+  res.json({ EquipmentModel });
+});
 
 module.exports = router;
