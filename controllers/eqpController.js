13281a526cb65d1cf1a54a668b7e525c6dd0d0b7
Rivers Marc <m.t.rivers@hotmail.com>
2021-03-17 12:59:16 -0500

    added heroku file

--- controllers/eqpController.js
+++ controllers/eqpController.js
@@ -1,56 +1,56 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-const EqpModel = require("../models").Eqp;
+// const EqpModel = require("../models").Eqp;
 
-  // Get ALL Equipment
-  router.get("/", async(req, res)=>{
-    console.log("API Working")
-    let allEqp = await EqpModel.findAll();
+//   // Get ALL Equipment
+//   router.get("/", async(req, res)=>{
+//     console.log("API Working")
+//     let allEqp = await EqpModel.findAll();
   
-    res.json({ allEqp });
-  })
-
-  // Get Single Piece of Equipment
-  router.get("/:id", async (req, res) => {
-    let equip = await EqpModel.findByPk(req.params.id);
-    res.json({ equip });
-  });
-
-  // UPDATE Equipment
-router.put("/:id", async (req, res) => {
-  let equip = await EqpModel.update(req.body, {
-    where: { id: req.params.id },
-    returning: true,
-  });
-  console.log("updated")
-  res.json({ equip });
-});
-
-// // Create Equipment
-// router.post("/", async (req, res) => {
-//   let equip = await EquipmentModel.create(req.body).then((response)=>{
-//     res.json({ response })
-//     console.log(response)
-//     console.log(req.body)
-
+//     res.json({ allEqp });
 //   })
-//   ;
+
+//   // Get Single Piece of Equipment
+//   router.get("/:id", async (req, res) => {
+//     let equip = await EqpModel.findByPk(req.params.id);
+//     res.json({ equip });
+//   });
+
+//   // UPDATE Equipment
+// router.put("/:id", async (req, res) => {
+//   let equip = await EqpModel.update(req.body, {
+//     where: { id: req.params.id },
+//     returning: true,
+//   });
+//   console.log("updated")
+//   res.json({ equip });
 // });
 
-// create equipment
-router.post("/", async (req, res) => {
-  let equip = await EqpModel.create(req.body);
-  res.json({ equip })
-})
+// // // Create Equipment
+// // router.post("/", async (req, res) => {
+// //   let equip = await EquipmentModel.create(req.body).then((response)=>{
+// //     res.json({ response })
+// //     console.log(response)
+// //     console.log(req.body)
+
+// //   })
+// //   ;
+// // });
+
+// // create equipment
+// router.post("/", async (req, res) => {
+//   let equip = await EqpModel.create(req.body);
+//   res.json({ equip })
+// })
 
 
 
-// Delete Equipment
-router.delete("/:id", async (req, res) => {
-  await EqpModel.destroy({ where: { id: req.params.id }});
-  res.json({ message: `equipment with id ${req.params.id} was deleted` });
-});
+// // Delete Equipment
+// router.delete("/:id", async (req, res) => {
+//   await EqpModel.destroy({ where: { id: req.params.id }});
+//   res.json({ message: `equipment with id ${req.params.id} was deleted` });
+// });
 
 
-module.exports = router;
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const express = require('express'); //from documentation: express is function
-const app = express();//app is an object
+const express = require('express');
+const app = express();
 const methodOverride = require("method-override");
+const cors = require("cors");
 
 app.use(methodOverride("_method"));
 app.use(express.json());
+app.use(cors());
 
-// app.get('/', (req, res) => {
-//     res.json('App is talking');
-// });
+
 
 app.use("/api/equipment", require("./controllers/equipmentController.js"));
 
-app.use("/api/eqp", require("./controllers/eqpController.js"));
 
-app.listen(3000, () => {
-    console.log("Nodemon listening");
-  });
+
+
     
